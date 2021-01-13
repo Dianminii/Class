cefcb6ad5a9f4ec7916106bc89d08eab3b428a88
Dianminii <74170971+Dianminii@users.noreply.github.com>
2021-01-13 09:43:07 +0700

    Add files via upload

---
+++ Class/src/Class/TI3A.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TI3A {
+    String namaTeman;
+    int nim; 
+    
+public TI3A(String nama, int nim) {
+    namaTeman = nama;
+    nim = nim;
+    
+}
+}
